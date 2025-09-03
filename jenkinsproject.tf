c618e0a852824ff3af9c4725a9dc18a4684d0fcd
Narendark790 <koletinarendar7@gmail.com>
2025-09-03 20:52:59 +0530

    Update jenkinsproject.tf

--- jenkinsproject.tf
+++ jenkinsproject.tf
@@ -2,12 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_vpc" "existing" {
+  id = "vpc-0d70179c349e98540"
+}
+
+data "aws_subnet" "existing" {
+  id = "subnet-09e452b7e0205f53b"
+}
+
 resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0360c520857e3138f"
   instance_type          = "t2.medium"
   key_name               = "kkeypair"
+  subnet_id              = data.aws_subnet.existing.id
   vpc_security_group_ids = ["sg-0335c4fe474fed430"]
+
   tags = {
     Name = var.instance_names[count.index]
   }
